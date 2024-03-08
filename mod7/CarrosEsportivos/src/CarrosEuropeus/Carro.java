a52001395f75de442dad2a49d0f9570579afe53e
DR1V3R15 <jrenan53@gmail.com>
2024-03-08 11:10:29 +0100

    Tarefa do modulo 7.

---
+++ mod7/CarrosEsportivos/src/CarrosEuropeus/Carro.java
@@ -0,0 +1,71 @@
+package CarrosEuropeus;
+
+/**
+ * @author João Renan
+ *
+ * Este arquivo é designado apenas para carros esportivos Europeus!
+ */
+public class Carro {
+
+    public String marcaDoVeiculo;
+
+    public String modeloDoVeiculo;
+
+    public int anoDoVeiculo;
+
+    public int valorDoVeiculo;
+
+    public float cilindradaDoVeiculo;
+
+    public float consumoDeCombustivelDoVeiculo;
+
+    public String getMarcaDoVeiculo() {
+        return marcaDoVeiculo;
+    }
+
+    public void setMarcaDoVeiculo(String marcaDoVeiculo) {
+        this.marcaDoVeiculo = marcaDoVeiculo;
+    }
+
+    public String getModeloDoVeiculo() {
+        return modeloDoVeiculo;
+    }
+
+    public void setModeloDoVeiculo(String modeloDoVeiculo) {
+        this.modeloDoVeiculo = modeloDoVeiculo;
+    }
+
+    public int getAnoDoVeiculo() {
+        return anoDoVeiculo;
+    }
+
+    public void setAnoDoVeiculo(int anoDoVeiculo) {
+        this.anoDoVeiculo = anoDoVeiculo;
+    }
+
+    public int getValorDoVeiculo() {
+        return valorDoVeiculo;
+    }
+
+    public void setValorDoVeiculo(int valorDoVeiculo) {
+        this.valorDoVeiculo = valorDoVeiculo;
+    }
+
+    public float getCilindradaDoVeiculo() {
+        return cilindradaDoVeiculo;
+    }
+
+    public void setCilindradaDoVeiculo(float cilindradaDoVeiculo) {
+        this.cilindradaDoVeiculo = cilindradaDoVeiculo;
+    }
+
+    public float getConsumoDeCombustivelDoVeiculo() {
+        return consumoDeCombustivelDoVeiculo;
+    }
+
+    public void setConsumoDeCombustivelDoVeiculo(float consumoDeCombustivelDoVeiculo) {
+        this.consumoDeCombustivelDoVeiculo = consumoDeCombustivelDoVeiculo;
+    }
+}
+
+
