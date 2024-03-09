12eb0a8c90d559bde5969dd65950b686ab8c001c
DR1V3R15 <jrenan53@gmail.com>
2024-03-09 06:31:36 +0100

    .

---
+++ modulo 8/TarefaCalculoMedia/CalculoDeMedia/src/LeituraDasNotas.java
@@ -0,0 +1,26 @@
+/**
+ * @author João Renan
+ *
+ * Exercício do Módulo 8 da EBAC.
+ */
+public class LeituraDasNotas {
+
+    public static void main(String args[]) {
+
+        calculoDaMedia();
+
+    }
+
+    public static void calculoDaMedia() {
+
+        int nota1 = 7;
+        int nota2 = 6;
+        int nota3 = 10;
+        int nota4 = 9;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(media);
+
+    }
+}
